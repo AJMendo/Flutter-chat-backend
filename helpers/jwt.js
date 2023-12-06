865c454603c00c62db5cfb58927a52e4826cc0d5
Antonio José Mendoza <mendojimenez@gmail.com>
2023-12-06 17:07:16 +0100

    Sección 8 acabada

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const generarJWT = ( uid ) => {
 
     return new Promise( (resolve, reject) => {
-        const payload = ( uid );
+        const payload = {uid};
 
         jwt.sign( payload, process.env.JWT_KEY, {
             expiresIn: '24h'
